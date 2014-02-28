1dcb408d308b890562e7cde51f8705156dcdce0a
hunterhector <hunterhector@gmail.com>
2014-02-28 12:36:19 -0500

    Complete the AgigaArgument class, and update README

--- src/edu/cmu/cs/lti/gigascript/agiga/AgigaArgument.java
+++ src/edu/cmu/cs/lti/gigascript/agiga/AgigaArgument.java
@@ -1,5 +1,8 @@
 package edu.cmu.cs.lti.gigascript.agiga;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import java.util.List;
 
 /**
@@ -8,18 +11,20 @@ import java.util.List;
 public class AgigaArgument {
     private int keywordTokenIndex;
     private int sentenceIndex;
+    private int documentIndex;
 
     // store alternative forms of the argument in order to make generization possible
     // for example, we could save the entity type of the argument here
     private List<String> alternativeForms;
 
-    public AgigaArgument(){
+    public AgigaArgument() {
 
     }
 
-    public AgigaArgument(int keywordTokenIndex, int sentenceIndex){
+    public AgigaArgument(int documentIndex, int sentenceIndex, int keywordTokenIndex) {
         this.keywordTokenIndex = keywordTokenIndex;
         this.sentenceIndex = sentenceIndex;
+        this.documentIndex = documentIndex;
     }
 
 
@@ -46,4 +51,30 @@ public class AgigaArgument {
     public void setAlternativeForms(List<String> alternativeForms) {
         this.alternativeForms = alternativeForms;
     }
+
+    public int getDocumentIndex() {
+        return documentIndex;
+    }
+
+    public void setDocumentIndex(int documentIndex) {
+        this.documentIndex = documentIndex;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj == this)
+            return true;
+        if (!(obj instanceof AgigaArgument))
+            return false;
+
+        AgigaArgument rhs = (AgigaArgument) obj;
+        return new EqualsBuilder().append(keywordTokenIndex, rhs.keywordTokenIndex).append(sentenceIndex, rhs.sentenceIndex).append(documentIndex,rhs.documentIndex).isEquals();
+    }
+
+    public int hashCode(){
+        return new HashCodeBuilder(17,31).append(keywordTokenIndex).append(sentenceIndex).append(documentIndex).toHashCode();
+    }
+
+
 }
