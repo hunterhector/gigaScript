ada68e2aa9e7381886a6cf4657660032168fec26
hunterhector <hunterhector@gmail.com>
2014-03-12 17:22:58 -0400

    Add a empty class to read the storage

---
+++ src/edu/cmu/cs/lti/gigascript/io/GigaStorageReader.java
@@ -0,0 +1,12 @@
+package edu.cmu.cs.lti.gigascript.io;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Hector, Zhengzhong Liu
+ * Date: 3/12/14
+ * Time: 2:17 PM
+ */
+public class GigaStorageReader {
+
+
+}
